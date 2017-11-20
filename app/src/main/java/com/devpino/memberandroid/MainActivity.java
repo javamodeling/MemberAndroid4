5e7b08aae66e7043a3d412d1b8862713ca5f1daf
Ritchie Ko <javamodeling@gmail.com>
2017-11-20 16:56:10 +0900

    new change

--- app/src/main/java/com/devpino/memberandroid/MainActivity.java
+++ app/src/main/java/com/devpino/memberandroid/MainActivity.java
@@ -1,23 +1,13 @@
 package com.devpino.memberandroid;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Member> members = new ArrayList<>();
 
-    private ListView listView;
+    private RecyclerView recyclerView;
 
-    private MemberArrayAdapter adapter = null;
+    private RecyclerView.Adapter memberAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView)findViewById(R.id.listview);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerViewMembers);
 
-        listView.setOnItemClickListener(onItemClickListenerListView);
+        recyclerView.setHasFixedSize(true);
 
         requestPermission();
     }
@@ -52,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         loadData();
-
-        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -88,97 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
         memberDbAdapter.close();
 
-        adapter = new MemberArrayAdapter(this, members);
+        memberAdapter = new MemberAdapter(members);
 
-        listView.setAdapter(adapter);
-
+        recyclerView.setAdapter(memberAdapter);
 
     }
 
-
-    private class MemberArrayAdapter extends ArrayAdapter<String> {
-
-        private final Activity context;
-
-        private final List<Member> members;
-
-        class ViewHolder {
-
-            public ImageView photo;
-            public TextView name;
-            public TextView email;
-        }
-
-        public MemberArrayAdapter(Activity context, List<Member> members) {
-
-            super(context, R.layout.row_layout);
-
-            this.context = context;
-            this.members = members;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            View rowView = convertView;
-
-            // reuse views
-            if (rowView == null) {
-
-                LayoutInflater inflater = context.getLayoutInflater();
-
-                rowView = inflater.inflate(R.layout.row2_layout, null);
-
-                // configure view holder
-                ViewHolder viewHolder = new ViewHolder();
-
-                viewHolder.photo = (ImageView) rowView.findViewById(R.id.imageViewPhoto);
-
-                viewHolder.name = (TextView) rowView.findViewById(R.id.textViewName);
-
-                viewHolder.email = (TextView) rowView.findViewById(R.id.textViewEmail);
-
-                rowView.setTag(viewHolder);
-            }
-
-            // fill data
-            ViewHolder holder = (ViewHolder) rowView.getTag();
-
-            Member member = members.get(position);
-
-            File imgFile = new  File(member.getPhotoUrl());
-
-            if(member.getPhotoUrl() != null) {
-
-                holder.photo.setImageURI(Uri.parse(member.getPhotoUrl()));
-            }
-
-            holder.name.setText(member.getMemberName());
-            holder.email.setText(member.getEmail());
-
-            return rowView;
-        }
-
-        @Override
-        public int getCount() {
-
-            return members.size();
-        }
-    }
-
-    AdapterView.OnItemClickListener onItemClickListenerListView = new AdapterView.OnItemClickListener() {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-
-            Member member = members.get(position);
-
-            long no = member.getNo();
-
-            Intent intent = new Intent(MainActivity.this, MemberViewActivity.class);
-            intent.putExtra("no", no);
-
-            startActivity(intent);
-        }
-    };
 }
